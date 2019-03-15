175efe919a69805942029521c21404b96983f1d3
zhifeng ou <zhifengou@hotmail.com>
2019-03-15 17:42:16 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // use this to copy code snippets or us your browser's console
+function justInvoke(callback) {
+  return callback();
+}
+ function setThisWithCall (callback, name, age) {
+  return callback.call(name, age);
+}
+ function setThisWithApply (callback, name, [age, hairColor]) {
+  return callback.apply(name, [age, hairColor]);
+}
+ function returnNewFunctionOf (fn, name) {
+  let newFunction = fn.bind(name);
+  return newFunction;
+}
